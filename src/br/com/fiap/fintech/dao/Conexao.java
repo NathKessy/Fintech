d73ed32994491a71b04985538cfd6c0468f24ecf
Nathalie Kessy <nathalienkm@gmail.com>
2023-10-21 11:38:44 -0300

    Iniciando Projeto

---
+++ src/br/com/fiap/fintech/dao/Conexao.java
@@ -0,0 +1,31 @@
+package br.com.fiap.fintech.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+	private static final String USUARIO = "RM99808";
+	private static final String SENHA = "041098";
+	
+	public static Connection abrirConexao() {
+		
+		Connection conexao = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+			
+			System.out.println("Sucelso!!!");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Classe não encontrada!");
+		} catch (SQLException e) {
+			System.out.println("Erro de conexão!");
+		}
+		
+		return conexao;
+	}
+}
