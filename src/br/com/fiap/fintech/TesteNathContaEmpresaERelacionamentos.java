639144cf1df90f149ecd75c5a7e175551731698f
gustavo.araujo <gustavo.aaraujo@totvs.com.br>
2023-10-26 13:22:14 -0300

    Ajustando commits com falha

--- src/br/com/fiap/fintech/TesteNathContaEmpresaERelacionamentos.java
+++ src/br/com/fiap/fintech/TesteNathContaEmpresaERelacionamentos.java
@@ -4,27 +4,40 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 
 import br.com.fiap.fintech.dao.ContaEmpresaDAO;
+import br.com.fiap.fintech.dao.SaldoDAO;
 import br.com.fiap.fintech.dao.UsuarioDAO;
 import br.com.fiap.fintech.model.ContaEmpresa;
+import br.com.fiap.fintech.model.Saldo;
 import br.com.fiap.fintech.model.Usuario;
 import br.com.fiap.fintech.model.enums.TipoContaEnum;
+import br.com.fiap.fintech.model.enums.TipoMoedaEnum;
 
 public class TesteNathContaEmpresaERelacionamentos {
 	
 	public static void main(String[] args) throws SQLException {
 		
+		SaldoDAO saldoDao = new SaldoDAO();
 		UsuarioDAO usuarioDao = new UsuarioDAO();
 		ContaEmpresaDAO contaEmpresaDao = new ContaEmpresaDAO();
 		
-		Usuario userDb = usuarioDao.getById(21);
+		Usuario userDb = usuarioDao.getById(1);
 		
-		ContaEmpresa contaEmpresa = new ContaEmpresa(2, userDb, "101", TipoContaEnum.CONTA_PREMIUM, true,
+		ContaEmpresa contaEmpresa = new ContaEmpresa(1, userDb, "101", TipoContaEnum.CONTA_PREMIUM, true,
 				LocalDate.now());
 		contaEmpresaDao.adicionar(contaEmpresa);
 		
 		System.out.println("\nListando todas contas empresas registradas:");
 		for (ContaEmpresa contEmpresa : contaEmpresaDao.getAll()) {
 			System.out.println(contEmpresa);
+		}
+		
+		System.out.println("\n*** Saldo ***");
+		Saldo saldo = new Saldo(1, contaEmpresa, 300.0, LocalDate.now(),TipoMoedaEnum.EURO);
+		saldoDao.adicionar(saldo);
+
+		System.out.println("\nListando todos os Saldos registrados:");
+		for (Saldo saldoLista : saldoDao.getAll()) {
+			System.out.println(saldoLista);
 		}
 	}
 
