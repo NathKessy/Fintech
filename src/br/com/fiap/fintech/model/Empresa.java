c3a4ced02ee86bfaaf87eb375a9f5f78bf0acc95
gustavo.araujo <gustavo.aaraujo@totvs.com.br>
2023-10-25 21:38:30 -0300

    Ajuste em EnderecoDao

--- src/br/com/fiap/fintech/model/Empresa.java
+++ src/br/com/fiap/fintech/model/Empresa.java
@@ -16,16 +16,14 @@ public class Empresa {
 
 	public Empresa() {
 	}
-	
 
 	public Empresa(Integer id) {
 		super();
 		this.id = id;
 	}
 
-
 	public Empresa(Integer id, DocumentosSocios documentosSocios, String razaoSocial, String nomeFantasia, String cnpj,
-			double capital_empresa, String cep, String telefone, String email, String endereco, double faturamento) {
+			double capital_empresa, String cep, String telefone, String email, Endereco endereco, double faturamento) {
 		super();
 		this.id = id;
 		this.documentosSocios = documentosSocios;
@@ -40,85 +38,106 @@ public class Empresa {
 		this.faturamento = faturamento;
 	}
 
+
 	public Integer getId() {
 		return id;
 	}
+
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+
 	public DocumentosSocios getDocumentosSocios() {
 		return documentosSocios;
 	}
+
 
 	public void setDocumentosSocios(DocumentosSocios documentosSocios) {
 		this.documentosSocios = documentosSocios;
 	}
 
+
 	public String getRazaoSocial() {
 		return razaoSocial;
 	}
+
 
 	public void setRazaoSocial(String razaoSocial) {
 		this.razaoSocial = razaoSocial;
 	}
 
+
 	public String getNomeFantasia() {
 		return nomeFantasia;
 	}
+
 
 	public void setNomeFantasia(String nomeFantasia) {
 		this.nomeFantasia = nomeFantasia;
 	}
 
+
 	public String getCnpj() {
 		return cnpj;
 	}
+
 
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
 
+
 	public double getCapital_empresa() {
 		return capital_empresa;
 	}
+
 
 	public void setCapital_empresa(double capital_empresa) {
 		this.capital_empresa = capital_empresa;
 	}
 
+
 	public String getCep() {
 		return cep;
 	}
+
 
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
 
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
 
+
 	public String getEmail() {
 		return email;
 	}
+
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+
 	public Endereco getEndereco() {
 		return endereco;
 	}
 
+
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+
 
 	public double getFaturamento() {
 		return faturamento;
